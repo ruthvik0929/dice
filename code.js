40c441ed2dbc459ada3feca567fe6f71c0525a18
ruthvik0929 <141345866+ruthvik0929@users.noreply.github.com>
2023-12-14 17:17:55 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen8");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("button4", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("button5", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screen1");
+});
